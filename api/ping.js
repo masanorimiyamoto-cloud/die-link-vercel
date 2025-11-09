156c8ce3126fe9f8556d96537bb8ed9d1931b151
masanorimiyamoto-cloud <mikekoro777@gmail.com>
2025-11-09 12:25:02 +0900

    re

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-﻿export const config = { runtime: "edge" };
-
-export default async function handler(req) {
-  return new Response(JSON.stringify({ ok: true, pong: Date.now() }, null, 2), {
-    status: 200,
-    headers: { "content-type": "application/json; charset=utf-8" }
-  });
-}
